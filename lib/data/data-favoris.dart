35e11f9f47ed16a53c2886dd8c4c14afd8378e69
Rae62 <89451474+Rae62@users.noreply.github.com>
2022-06-30 13:59:21 +0200

    Ludovic
    Structuration du dossier

---
+++ lib/data/data-favoris.dart
@@ -0,0 +1,36 @@
+import 'package:muselight/models/todoModel.dart';
+
+List<ToDo> tovisit = [
+  ToDo(
+    image:
+        'https://media.istockphoto.com/photos/interior-of-a-pub-picture-id1148223648?s=612x612',
+    name: 'Restaurant 1',
+    id: 'p1',
+    city: 'Thailandais',
+    checked: false,
+  ),
+  ToDo(
+    image:
+        'https://cdn.pixabay.com/photo/2016/11/29/12/54/cafe-1869656_960_720.jpg',
+    name: 'Restaurant 2',
+    id: 'p2',
+    city: 'Japonnais',
+    checked: false,
+  ),
+  ToDo(
+    image:
+        'https://media.istockphoto.com/photos/empty-cafe-interior-with-chairs-and-tables-picture-id1286692956?s=612x612',
+    name: 'Restaurant 3',
+    id: 'p3',
+    city: 'Marocainne',
+    checked: false,
+  ),
+  ToDo(
+    image:
+        'https://media.istockphoto.com/photos/empty-cafe-interior-with-chairs-and-tables-picture-id1286692956?s=612x612',
+    name: 'Restaurant 4',
+    id: 'p4',
+    city: 'Indien',
+    checked: false,
+  ),
+];
---
+++ lib/models/todoModel.dart
@@ -0,0 +1,15 @@
+class ToDo {
+  String name;
+  String image;
+  String id;
+  String city;
+  bool checked;
+
+  ToDo({
+    required this.name,
+    required this.image,
+    required this.id,
+    required this.city,
+    required this.checked,
+  });
+}
